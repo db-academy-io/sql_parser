29caf83e3cf6628a53198efe232711c2b63ec1df
Almaz Murzabekov <almaz@murzabekov.net>
2024-11-11 19:19:07 +0000

    minor improvement on the ast

--- sql-parser/src/ast/alter.rs
+++ sql-parser/src/ast/alter.rs
@@ -1,3 +1,1 @@
 pub struct AlterTableStatement {}
-
-pub struct RenameTableStatement {}
--- sql-parser/src/ast/mod.rs
+++ sql-parser/src/ast/mod.rs
@@ -9,7 +9,7 @@ mod sqlite;
 mod trx;
 mod update;
 
-use alter::{AlterTableStatement, RenameTableStatement};
+use alter::AlterTableStatement;
 use create::{
     CreateIndexStatement, CreateTableStatement, CreateTriggerStatement, CreateViewStatement,
     CreateVirtualTableStatement,
@@ -29,44 +29,63 @@ use trx::{
 };
 use update::UpdateStatement;
 
-// Top-level AST node representing any SQLite statement
+/// Top-level AST node representing any SQLite statement
 pub enum Statement {
-    // Data Query Language (DQL)
+    /// Data Query Language (DQL), see [SelectStatement]
     Select(SelectStatement),
 
-    // Data Manipulation Language (DML)
+    /// Data Manipulation Language (DML), see [InsertStatement]
     Insert(InsertStatement),
+    /// Data Manipulation Language (DML), see [UpdateStatement]
     Update(UpdateStatement),
+    /// Data Manipulation Language (DML), see [DeleteStatement]
     Delete(DeleteStatement),
 
-    // Data Definition Language (DDL)
+    /// Data Definition Language (DDL), see [CreateTableStatement]
     CreateTable(CreateTableStatement),
+    /// Data Definition Language (DDL), see [AlterTableStatement]
     AlterTable(AlterTableStatement),
+    /// Data Definition Language (DDL), see [DropTableStatement]
     DropTable(DropTableStatement),
+    /// Data Definition Language (DDL), see [CreateIndexStatement]
     CreateIndex(CreateIndexStatement),
+    /// Data Definition Language (DDL), see [DropIndexStatement]
     DropIndex(DropIndexStatement),
+    /// Data Definition Language (DDL), see [CreateViewStatement]
     CreateView(CreateViewStatement),
+    /// Data Definition Language (DDL), see [DropViewStatement]
     DropView(DropViewStatement),
+    /// Data Definition Language (DDL), see [CreateTriggerStatement]
     CreateTrigger(CreateTriggerStatement),
+    /// Data Definition Language (DDL), see [DropTriggerStatement]
     DropTrigger(DropTriggerStatement),
+    /// Data Definition Language (DDL), see [CreateVirtualTableStatement]
     CreateVirtualTable(CreateVirtualTableStatement),
-    RenameTable(RenameTableStatement),
 
-    // Transaction Control Language (TCL)
+    /// Transaction Control Language (TCL), see [BeginTransactionStatement]
     BeginTransaction(BeginTransactionStatement),
+    /// Transaction Control Language (TCL), see [CommitTransactionStatement]
     CommitTransaction(CommitTransactionStatement),
+    /// Transaction Control Language (TCL), see [RollbackTransactionStatement]
     RollbackTransaction(RollbackTransactionStatement),
+    /// Transaction Control Language (TCL), see [SavepointStatement]
     Savepoint(SavepointStatement),
+    /// Transaction Control Language (TCL), see [SavepointReleaseStatement]
     Release(SavepointReleaseStatement),
 
-    // Administrative statements
+    /// SQLite Administrative statements, see [PragmaStatement]
     Pragma(PragmaStatement),
+    /// SQLite Administrative statements, see [AttachStatement]
     Attach(AttachStatement),
+    /// SQLite Administrative statements, see [DetachStatement]
     Detach(DetachStatement),
+    /// SQLite Administrative statements, see [VacuumStatement]
     Vacuum(VacuumStatement),
+    /// SQLite Administrative statements, see [AnalyzeStatement]
     Analyze(AnalyzeStatement),
+    /// SQLite Administrative statements, see [ReindexStatement]
     Reindex(ReindexStatement),
 
-    // EXPLAIN statement (can wrap another statement)
+    /// EXPLAIN statement (can wrap another statement), see [ExplainStatement]
     Explain(ExplainStatement),
 }
