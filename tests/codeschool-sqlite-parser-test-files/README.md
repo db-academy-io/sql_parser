20e1ab7975ada2905f9e6fe5aa0382a30c8e4c91
Almaz Murzabekov <almaz@murzabekov.net>
2024-12-06 12:22:45 +0100

    sql-parser: codeschool sqlite parser test files

---
+++ tests/codeschool-sqlite-parser-test-files/README.md
@@ -0,0 +1,6 @@
+## Test sql queries from codeschool sqlite parser
+
+The queries under `sql/` folder are taken from the [Codeschool SQLite Parser](https://github.com/codeschool/sqlite-parser/tree/master) repository.
+
+
+Current test pass rate is 37.26%
---
+++ tests/codeschool-sqlite-parser-test-files/main.rs
@@ -0,0 +1,49 @@
+use std::{fs, path::Path};
+
+use sql_parser::Parser;
+
+#[test]
+fn run_codeschool_sqlite_parser_test() {
+    let test_set_path = Path::new("tests/codeschool-sqlite-parser-test-files/sql");
+
+    let mut total_tests = 0;
+    let mut passed_test = 0;
+
+    for group in fs::read_dir(test_set_path).expect("Failed to read test_set directory") {
+        let group = group.expect("Failed to read directory entry").path();
+        if !group.is_dir() {
+            continue;
+        }
+
+        for entry in fs::read_dir(group).expect("Failed to read test_set directory") {
+            let file = entry.expect("Failed to read directory entry").path();
+
+            if file.extension().and_then(|ext| ext.to_str()) == Some("sql") {
+                let parsing_result = std::panic::catch_unwind(|| run_test(&file));
+
+                if parsing_result.is_ok() && parsing_result.unwrap() {
+                    passed_test += 1;
+                }
+                total_tests += 1;
+            }
+        }
+    }
+
+    println!("Tested {} SQL files", total_tests);
+    println!(
+        "Passed {} SQL files, {} failed",
+        passed_test,
+        total_tests - passed_test
+    );
+    println!(
+        "Passed {:.2}% of the tests",
+        passed_test as f64 / total_tests as f64 * 100.0
+    );
+}
+
+fn run_test(file: &Path) -> bool {
+    let sql_content = fs::read_to_string(file).expect("Failed to read file");
+    let mut parser = Parser::from(sql_content.as_str());
+    let statement = parser.parse_statement();
+    statement.is_ok()
+}
