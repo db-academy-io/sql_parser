8dd21e769e9a93036277bf849bd4d02da919caa0
Almaz Murzabekov <almaz@murzabekov.net>
2024-12-06 12:08:15 +0100

    sql-parser: expression parser (expression from expressions)

--- src/parser/expression/mod.rs
+++ src/parser/expression/mod.rs
@@ -1,10 +1,10 @@
 mod function;
 
 use crate::{
-    parser::select::SelectStatementParser, BetweenExpression, BinaryMatchingExpression, BinaryOp,
-    CaseExpression, DataType, EscapeExpression, ExistsStatement, Expression, Identifier, Keyword,
-    LikeExpressionType, LiteralValue, Parser, ParsingError, RaiseFunction, TokenType,
-    UnaryMatchingExpression, UnaryOp, WhenExpression,
+    parser::select::SelectStatementParser, AnIsExpression, BetweenExpression,
+    BinaryMatchingExpression, BinaryOp, CaseExpression, DataType, EscapeExpression,
+    ExistsStatement, Expression, Identifier, Keyword, LikeExpressionType, LiteralValue, Parser,
+    ParsingError, RaiseFunction, TokenType, UnaryMatchingExpression, UnaryOp, WhenExpression,
 };
 
 use function::FunctionParser;
@@ -86,8 +86,10 @@ pub trait ExpressionParser {
     ) -> Result<Expression, ParsingError>;
 
     /// Parse an identifier
-    /// Parse an identifier
     fn parse_identifier(&mut self) -> Result<Identifier, ParsingError>;
+
+    /// Parse an `$expr IS $expr` expression
+    fn parse_is_expression(&mut self, expression: Expression) -> Result<Expression, ParsingError>;
 
     /// Parse an expression using Pratt's parsing algorithm
     /// `rbp` is the right binding power of the current operator
@@ -221,6 +223,10 @@ impl<'a> ExpressionParser for Parser<'a> {
                 Keyword::Glob | Keyword::Regexp | Keyword::Match => {
                     // No need to consume the keyword, as the regexp match parser will handle it
                     return self.parse_regexp_match_expression(expression, false);
+                }
+                Keyword::Is => {
+                    // No need to consume the IS keyword, as the is parser will handle it
+                    return self.parse_is_expression(expression);
                 }
                 _ => {}
             }
@@ -681,6 +687,37 @@ impl<'a> ExpressionParser for Parser<'a> {
                 matching_expression,
             ))
         }
+    }
+
+    fn parse_is_expression(&mut self, expression: Expression) -> Result<Expression, ParsingError> {
+        self.consume_keyword(Keyword::Is)?;
+
+        let is_not = self.consume_keyword(Keyword::Not).is_ok();
+        let mut distinct = false;
+
+        if let Ok(Keyword::Distinct) = self.peek_as_keyword() {
+            self.consume_keyword(Keyword::Distinct)?;
+            distinct = true;
+
+            // The FROM keyword is mandatory after the DISTINCT keyword
+            self.consume_keyword(Keyword::From)?;
+        }
+
+        let is_expression = BinaryMatchingExpression::Is(AnIsExpression {
+            expression: Box::new(self.parse_expression()?),
+            distinct,
+        });
+
+        let result = if is_not {
+            BinaryMatchingExpression::Not(Box::new(is_expression))
+        } else {
+            is_expression
+        };
+
+        Ok(Expression::BinaryMatchingExpression(
+            Box::new(expression),
+            result,
+        ))
     }
 }
 
@@ -1569,5 +1606,72 @@ mod regexp_match_expression_tests {
                 ),
             );
         }
+    }
+}
+
+#[cfg(test)]
+mod expression_from_expression_tests {
+    use crate::{AnIsExpression, BinaryMatchingExpression, Expression};
+
+    use super::test_utils::*;
+
+    fn expression_from_expression(
+        expression: Expression,
+        is_expression: Expression,
+        distinct: bool,
+        is_not: bool,
+    ) -> Expression {
+        let binary_matching_expression = if is_not {
+            BinaryMatchingExpression::Not(Box::new(BinaryMatchingExpression::Is(AnIsExpression {
+                expression: Box::new(is_expression),
+                distinct,
+            })))
+        } else {
+            BinaryMatchingExpression::Is(AnIsExpression {
+                expression: Box::new(is_expression),
+                distinct,
+            })
+        };
+
+        Expression::BinaryMatchingExpression(Box::new(expression), binary_matching_expression)
+    }
+
+    #[test]
+    fn test_expression_is_another_expression() {
+        run_sunny_day_test(
+            "SELECT 1 IS 1;",
+            &expression_from_expression(
+                numeric_literal_expression("1"),
+                numeric_literal_expression("1"),
+                false,
+                false,
+            ),
+        );
+    }
+
+    #[test]
+    fn test_expression_is_not_another_expression() {
+        run_sunny_day_test(
+            "SELECT 1 IS NOT 1;",
+            &expression_from_expression(
+                numeric_literal_expression("1"),
+                numeric_literal_expression("1"),
+                false,
+                true,
+            ),
+        );
+    }
+
+    #[test]
+    fn test_expression_is_distinct_another_expression() {
+        run_sunny_day_test(
+            "SELECT 1 IS DISTINCT FROM 1;",
+            &expression_from_expression(
+                numeric_literal_expression("1"),
+                numeric_literal_expression("1"),
+                true,
+                false,
+            ),
+        );
     }
 }
